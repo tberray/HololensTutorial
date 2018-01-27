eb83c047a62395778a9a74632740f71b99889ed2
Zach Posten <zposten@gmail.com>
2018-01-27 13:09:28 -0600

    Add block color script

---
+++ Assets/BlockColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var cubeColor = new Color(
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f)
+        );
+
+        this.GetComponent<Renderer>().material.color = cubeColor;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
